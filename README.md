e36996d0d8e8f4181b70dcdd3ea810d7d7ba44e3
andreaweb <deiainluv@gmail.com>
2018-08-12 16:18:26 -0300

    added comments to js

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1>Super Dario</h1>
+<h2>Simple sample game for interactive campaign</h2>
+
+<h3>To install and run the application</h3>
+
+<ul>
+	<li>Download it and run <code>npm install</code></li>
+	<li>Run <code>gulp watch</code> or <code>npm start</code> to see it on your browser</li>
+	<li><code>Gulp</code> is currently minifying css and js automatically and reloading the project on each change</li>
+</ul>
+
+<h3>What the app is</h3>
+
+<ul>
+	<li>Super Dario runs/walks forever, automatically</li>
+	<li>After you click "Play Now" an enemy will appear at the screen</li>
+	<li>You must jump to avoid the enemy or Super Dario will get hurt</li>
+	<li>If Super Dario is hurt, he becomes small. If he's hurt again, he dies</li>
+	<li>To jump, press any key or tap your screen if you're on your phone</li>
+</ul>
+
+
+<h3>How the app works</h3>
+
+<ul>
+	<li>Super Dario, the ground and the enemy move constantly through CSS animations</li>
+	<li>When the game starts, the position of the enemy (<code>offsetLeft</code>) is constantly compared with Super Dario's position</li>
+	<li>If Super Dario and the enemy share the same offsetLeft, but Super Dario is jumping (class <code>.up</code>), Super Dario isn't hurt</li>
+	<li>If Super Dario is hurt, JS checks if he has the <code>.small</code> class. If yes, he fades (dies) and the game 'freezes' (animations stop).</li>
+	<li>If no, he becomes small by the add of the <code>.small</code> class.</li>
+</ul>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let gameStart = document.getElementsByClassName("tap")[0];
 let gameOn = false;
 
+/*Game starts only when button is clicked*/
 gameStart.addEventListener("click", (event) => {
 	document.getElementsByClassName("container")[0].className = document.getElementsByClassName("container")[0].className.concat(' game-on');
 	gameStart.className = gameStart.className.concat(' display-none');
@@ -9,32 +10,35 @@ gameStart.addEventListener("click", (event) => {
 	startGame()
 })
 
+
 function startGame(){
+	/*check for collisions every 0.2 seconds*/
 	let timer = setInterval(checkCol, 200); 
 
 	function checkCol(){
+		/*...unless game is not on*/			
 		if(!gameOn){
 			return false;
+		}else{
+			let bulletPos = document.getElementsByClassName("bullet")[0].offsetLeft;
+			let darioPos = document.getElementsByClassName("dario")[0].offsetLeft;
+			let darioClasses = document.getElementsByClassName("dario")[0].className.split(' ');
+			let darioJump = darioClasses.indexOf('up');
+			let darioSmall = darioClasses.indexOf('small');
+
+			if(bulletPos > darioPos && bulletPos < darioPos+40){/*collision is happening*/
+				if(darioJump == -1 && darioSmall == -1){ /*dario did not jump (did not dodge bullet), dario is not small*/
+					console.log('Dario will become small')
+					document.getElementsByClassName("dario")[0].className = document.getElementsByClassName("dario")[0].className.concat(' small');
+					return; //avoid else triggering immediately
+				}else if(darioJump == -1 && darioSmall > -1){/*dario did not dodge bullet, dario is small*/
+					console.log('Dario will fade and game will freeze');
+					document.getElementsByClassName("dario")[0].className = document.getElementsByClassName("dario")[0].className.concat(' fade');
+					document.getElementsByTagName("body")[0].className = document.getElementsByTagName("body")[0].className.concat(' freeze');
+					gameOn = false;
+				}
+		    }
 		}
-
-		let bulletPos = document.getElementsByClassName("bullet")[0].offsetLeft;
-		let darioPos = document.getElementsByClassName("dario")[0].offsetLeft;
-		let darioClasses = document.getElementsByClassName("dario")[0].className.split(' ');
-		let darioJump = darioClasses.indexOf('up');
-		let darioSmall = darioClasses.indexOf('small');
-
-		if(bulletPos > darioPos && bulletPos < darioPos+40){
-			if(darioJump == -1 && darioSmall == -1){ /*dario did not jump (did not dodge bullet), dario is not small*/
-				console.log('Dario will become small')
-				document.getElementsByClassName("dario")[0].className = document.getElementsByClassName("dario")[0].className.concat(' small');
-				return; //avoid else triggering immediately
-			}else if(darioJump == -1 && darioSmall > -1){/*dario did not dodge bullet, dario is small*/
-				console.log('Dario will fade and game will freeze');
-				document.getElementsByClassName("dario")[0].className = document.getElementsByClassName("dario")[0].className.concat(' fade');
-				document.getElementsByTagName("body")[0].className = document.getElementsByTagName("body")[0].className.concat(' freeze');
-				gameOn = false;
-			}
-	    }
 	}
 
 	function jump(){
@@ -48,7 +52,7 @@ function startGame(){
 		}, 500);
 	}
 
-	// Dario jumps when any key is pressed
+	// Dario jumps when any key is pressed or if there's a tap on the touchscreen 
 	document.addEventListener("keypress", (event) => jump() );
 	document.addEventListener("touchend", (event) => jump() );
 }
